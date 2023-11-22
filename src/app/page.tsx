8d89d432faec1d82bcaac23e8d96fef28d5572bf
Indra Zulfi <me@indrazm.com>
2023-11-22 21:49:41 +0700

    feat: auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import { signOut, useSession } from 'next-auth/react';
+
+import { Login } from '@/components/Auth/Login';
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
-  return <div className="border-1 bg-rose-700 font-bold text-red-400 " />;
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      {session ? (
+        <div className="flex justify-between p-8">
+          <div className="text-lg font-bold">App.</div>
+          <div className="flex items-center gap-4">
+            <div>{session.user?.name}</div>
+            <img alt="" src={session.user?.image} width={40} height={40} className="rounded-full" />
+            <Button onClick={() => signOut()}>Logout</Button>
+          </div>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
 }
